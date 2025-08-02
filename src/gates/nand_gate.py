72507b3f3a8044291790eac37b89f9a624535c24
j341nono <kanakana.nono1224@gmail.com>
2025-08-03 00:39:55 +0900

    Implement NAND gate and its test

--- src/gates/nand_gate.py
+++ src/gates/nand_gate.py
@@ -1,3 +1,4 @@
-from and_gate import and_gate
+from src.gates.and_gate import and_gate
 
-def nand_gate
+def nand_gate(x:bool, y:bool) -> bool:
+    return not and_gate(x, y)
--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,2 @@
 set -x
-uv run -m tests.test_gates
+uv run -m tests.test_gates --nand_gate
